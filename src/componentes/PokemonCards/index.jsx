74fe817963380caee1e8614b992b041262391557
Henrique <henriquemendeseloi@gmail.com>
2025-11-16 18:36:45 -0300

    16-11

--- src/componentes/PokemonCards/index.jsx
+++ src/componentes/PokemonCards/index.jsx
@@ -5,24 +5,20 @@ import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
 import CardActionArea from '@mui/material/CardActionArea';
 
-export default function PokemonCard() {
+export default function PokemonCard({ name = 'Pokemon', image = '' }) {
   return (
     <Card sx={{ maxWidth: 345 }}>
       <CardActionArea>
         <CardMedia
           component="img"
           height="140"
-          image="/static/images/cards/contemplative-reptile.jpg"
-          alt="green iguana"
+          image={image}
+          alt={name}
         />
         <CardContent>
           <Typography gutterBottom variant="h5" component="div">
-            Lizard
+            {name}
           </Typography>
-          {/*<Typography variant="body2" sx={{ color: 'text.secondary' }}>
-            Lizards are a widespread group of squamate reptiles, with over 6,000
-            species, ranging across all continents except Antarctica?
-          </Typography>*/}
         </CardContent>
       </CardActionArea>
     </Card>
