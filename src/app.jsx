4cb1e622748fc2e33476fe065794c7fd23583bce
Henrique <henriquemendeseloi@gmail.com>
2025-11-23 19:25:43 -0300

    11-11

--- src/app.jsx
+++ src/app.jsx
@@ -1,24 +1,4 @@
-import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import Home from "./pages/Home";
-import Geracoes from "./pages/Geracoes";
-import Categorias from "./pages/Categorias"; // <--- Importe aqui
-
-function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/geracoes" element={<Geracoes />} />
-        {/* Adicione esta linha abaixo */}
-        <Route path="/categorias" element={<Categorias />} />
-      </Routes>
-    </BrowserRouter>
-  );
-}
-
-export default App;
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
